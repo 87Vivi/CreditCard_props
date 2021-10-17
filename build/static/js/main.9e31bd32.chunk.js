c79bac853b7692c6f8516a6cec1dcc2284c5368a
Viviana Castaldo <79976364+87Vivi@users.noreply.github.com>
2021-10-17 10:34:29 +0200

    Add files via upload

---
+++ build/static/js/main.9e31bd32.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp15-react-state"]=this["webpackJsonp15-react-state"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),a=c.n(s),i=c(0);var p=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"My React App"})})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.9e31bd32.chunk.js.map
